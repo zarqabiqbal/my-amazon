913b778ddb6447fb2bb5da83df9af12f7dc671f6
Zarqab Iqbal <zarqabiqbal@gmail.com>
2022-03-13 15:39:39 +0530

    add hyperlink on navbar

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,16 @@
-import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
+import {
+  AppBar,
+  Container,
+  Link,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
 import Head from 'next/head';
 import React from 'react';
 import my_style from '../utils/style';
+import NextLink from 'next/link';
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
   const classes = my_style();
   return (
     <div>
@@ -12,16 +19,27 @@ export default function Layout({children}) {
       </Head>
       <AppBar position="static" className={classes.navbar}>
         <Toolbar>
-          <Typography>amazon</Typography>
+          <NextLink href="/" passHref>
+            <Link>
+              <Typography className={classes.brand}>amazon</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href="/cart" passHref>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href="/login" passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
         </Toolbar>
       </AppBar>
-      <Container className={classes.main}>
-          {children}
-      </Container>
+      <Container className={classes.main}>{children}</Container>
       <footer className={classes.footer}>
-          <Typography>
-              All right reserved. Next Js App Developed by Zarqab
-          </Typography>
+        <Typography>
+          All right reserved. Next Js App Developed by Zarqab
+        </Typography>
       </footer>
     </div>
   );
--- utils/style.js
+++ utils/style.js
@@ -1,23 +1,30 @@
-import { makeStyles } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core';
 
 const my_style = makeStyles({
-    navbar : {
-        backgroundColor: '#203040',
-        '& a':{
-            color:'#ffffff',
-            marginLeft: 10,
-        }
+  navbar: {
+    backgroundColor: '#203040',
+    '& a': {
+      color: '#ffffff',
+      marginLeft: 10,
     },
-    main : {
-        minHeight:'80vh',
-        '& li' : {
-            padding: '5px',
-            fontWeight:'500',
-        },
+  },
+  main: {
+    minHeight: '80vh',
+    '& li': {
+      padding: '5px',
+      fontWeight: '500',
     },
-    footer : {
-        textAlign : 'center',
-    }
+  },
+  brand: {
+    fontWeight: 'bold',
+    fontSize: '1.5rem',
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  footer: {
+    textAlign: 'center',
+  },
 });
 
 export default my_style;
